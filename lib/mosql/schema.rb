591ff5361ce637f8ac80381e9718835decf231dd
Francisco Molina <francisco.molina@urbans.cl>
2021-01-04 03:47:16 -0300

    cambio tipo error

--- lib/mosql/schema.rb
+++ lib/mosql/schema.rb
@@ -304,7 +304,7 @@ module MoSQL
         begin
           pg.get_result.check
         rescue PGError => e
-          log.error("query error : #{sql}")
+          log.debug("query error : #{sql}")
           db.send(:raise_error, e)
         end
       end
